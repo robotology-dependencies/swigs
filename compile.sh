a54e24d5b60613703189bdbdceecf5353d48c9db
Daniele E. Domenichelli <daniele.domenichelli@iit.it>
2016-02-25 14:35:52 +0100

    A few improvements to the package generation

--- compile.sh
+++ compile.sh
@@ -2,14 +2,6 @@
 
 set -e
 
-default_swig_versions="1.3.39 1.3.40 2.0.1 2.0.2 2.0.3 2.0.4 2.0.5 2.0.6 2.0.7 2.0.8 2.0.9 2.0.10 2.0.11 2.0.12 3.0.0 3.0.1 3.0.2"
-
-if [ -z "$SWIG_VERSIONS" ]; then
-    swig_versions="$default_swig_versions"
-else
-    swig_versions="$SWIG_VERSIONS"
-fi
-
 swig_base="$PWD/build/swigs"
 mkdir -p $swig_base
 
@@ -18,29 +10,46 @@ if [ ! -e swig ]; then
     cd swig
 else
     cd swig
-    git pull https://github.com/swig/swig
+    git fetch --prune
+    git reset --hard origin/master
 fi
 
-echo "Compiling all swig versions"
 if [ ! -e swig.spec.in ]; then
     echo "Failed to load swig"
     exit 1
 fi
 
+
+default_swig_versions="$(git for-each-ref --format="%(refname)" refs/tags/rel-*.*.* | sed 's|refs/tags/rel-||' | sort -V | grep -v "1.3.32\|1.3.33\|1.3.34\|1.3.35\|1.3.36\|beta")"
+
+if [ -z "$SWIG_VERSIONS" ]; then
+    swig_versions="$default_swig_versions"
+    echo "Compiling all swig versions"
+else
+    swig_versions="$SWIG_VERSIONS"
+    echo "Compiling swig versions: $swig_versions"
+fi
+
 for f in $swig_versions; do
     if [ ! -e $swig_base/$f ] ; then
-	echo $f
-	git clean -f -d
-	git checkout .
-	git checkout rel-$f
-	./autogen.sh
-	./configure --prefix=$swig_base/$f
-	make clean
-	make
-	make install
+        echo "Building swig version $f in $swig_base/$f"
+        echo $f
+        git clean -f -d
+        git checkout .
+        git checkout rel-$f
+        ./autogen.sh
+        ./configure --prefix=$swig_base/$f
+        make clean || true
+        make
+        make install
+    else
+        echo "swig version $f already built in $swig_base/$f"
     fi
 done
 
 cd $swig_base/..
 rm -f swigs.zip
-zip -r swigs swigs
+for f in $swig_versions; do
+    echo "adding swig version $f to the archive"
+    zip -qr swigs.zip swigs/$f
+done
